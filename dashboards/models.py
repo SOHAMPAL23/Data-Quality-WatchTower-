412a37a9275b450e6f686d072f13b1600a61eeb1
SOHAMPAL23 <palsoham074@gmail.com>
2025-12-04 02:06:36 +0530

    Deleted file

--- dashboards/models.py
+++
@@ -1,4 +0,0 @@
-from django.db import models
-
-# Dashboards app might not need specific models as it's primarily for displaying data
-# But we can add any dashboard-specific models here if needed in the future
